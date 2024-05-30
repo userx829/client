6a8e9c2556abc2eb0370cb044154aaba2c3973f5
userx829 <156603330+userx829@users.noreply.github.com>
2024-05-30 16:46:53 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -15,7 +15,7 @@ const Login = () => {
     console.log("clicked");
    
     try {
-      const response = await fetch("http://localhost:5000/api/login", {
+      const response = await fetch("https://server-s63i.onrender.com/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
